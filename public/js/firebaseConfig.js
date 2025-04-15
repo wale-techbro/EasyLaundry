3ccd1a8892ddb973b87e2478287283df2ecd7215
David Olayiwola <111184566+wale-techbro@users.noreply.github.com>
2025-04-15 08:44:47 +0100

    Create firebaseConfig.js

---
+++ public/js/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAj33z89WqPI9YMcakZDeNAX8riFCxNx0Q",
+    authDomain: "easylaundry-91ed7.firebaseapp.com",
+    projectId: "easylaundry-91ed7",
+    storageBucket: "easylaundry-91ed7.firebasestorage.app",
+    messagingSenderId: "894016437104",
+    appId: "1:894016437104:web:fd283fde1746b52687adb5",
+    measurementId: "G-8HSWYNRFM2"
+};
